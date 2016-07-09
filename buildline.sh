cf9610356e029ab9daa3ef91e656c6a41feaeb23
okuoku <mjt@cltn.org>
2016-07-09 19:45:51 +0900

    Remove unused scripts

--- buildline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-exec /cygdrive/c/prog/larceny-0.99-bin-native-ia32-win32/larceny.bat -path ../rapid-scheme -r7rs -program rapid-compiler-gambit.scm -- -I ../rapid-scheme -I ../rapid-scheme/share rapid-compiler-gambit.scm > core.scm
- 
