e04e884a8746448b0a86a47e1cf14be3d0036bd5
okuoku <mjt@cltn.org>
2016-07-10 09:30:55 +0900

    Use my own macro expander where possible

--- r7/_override.scm
+++ r7/_override.scm
@@ -1,16 +1,3 @@
-(define-macro (with-r7-override . body)
-  (define override
-    '(;; base
-      (raise raise:r7)
-      (with-exception-handler with-exception-handler:r7)
-      (string-copy string-copy:r7)
-      ;; process-context
-      (exit exit:r7)
-      ;; eval
-      (eval eval:r7)
-      ))
-  `(let ,override ,@body))
-
 (define (%%inject-r7-override form)
   (define override
     '(;; base
--- rt/values-utils.scm
+++ rt/values-utils.scm
@@ -1,16 +1,3 @@
-(define-macro (let-values cls . body)
-  (if (pair? cls)
-    (let ((a (car cls))
-          (d (cdr cls)))
-      (let ((frms (car a))
-            (init (cadr a)))
-        `(call-with-values
-           (lambda () ,init)
-           (lambda ,frms 
-             (let-values ,d ,@body)))))
-    `(let () ,@body)))
-
-;; FIXME: copy-paste
 (define (%%expand-let-values cls . body)
   (if (pair? cls)
     (let ((a (car cls))
@@ -23,14 +10,6 @@
              (let-values ,d ,@body)))))
     `(let () ,@body)))
 
-(define-macro (set!-values v* body)
-  (let ((xfrm (map gensym v*)))
-   `(call-with-values
-      (lambda () ,body)
-      (lambda ,xfrm 
-        ,@(map (lambda (frm x) `(set! ,frm ,x)) v* xfrm)))))
-
-;; FIXME: copy-paste
 (define (%%expand-set!-values v* body)
   (let ((xfrm (map gensym v*)))
    `(call-with-values
